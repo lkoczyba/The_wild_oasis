2de876ff80d74798abdb70260a05e5d5012040d0
lkoczyba <lukasz.koczyba@gmail.com>
2023-12-04 17:46:28 +0100

    setup react query

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vrwagcypjsilrnjuldsh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyd2FnY3lwanNpbHJuanVsZHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MzczMjcsImV4cCI6MjAxNzAxMzMyN30.mpCmMbP-L3paJw6X4sbUxbp1zEXYIylJRCO4qFKUSbo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
