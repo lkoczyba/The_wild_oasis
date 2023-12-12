d0f1292c509d8adfd97e041ec67b6b4f93c01223
lkoczyba <lukasz.koczyba@gmail.com>
2023-12-12 20:00:32 +0100

    use React hook Form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -56,5 +56,9 @@ const Button = styled.button`
   ${(props) => sizes[props.size]}
   ${(props) => variations[props.variation]}
 `;
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 
 export default Button;
